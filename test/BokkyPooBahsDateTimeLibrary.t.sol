96d6ea88150ed7e93895990ad8fafc8774ee4117
Elliot <elliotfriedman3@gmail.com>
2024-04-07 00:28:21 -0700

    test calendar
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

---
+++ test/BokkyPooBahsDateTimeLibrary.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+
+import {BokkyPooBahsDateTimeLibrary} from "src/calendar/BokkyPooBahsDateTimeLibrary.sol";
+
+contract TimeUnitTest is Test {
+    using BokkyPooBahsDateTimeLibrary for *;
+
+    function testHourNeverGt23(uint256 day) public {
+        assertLt(day.getHour(), 24, "Hour should be less than 24");
+    }
+
+    function testDayRange(uint256 day) public {
+        assertLt(
+            day.getDayOfWeek(),
+            8,
+            "Day of week should be less than or equal to 7"
+        );
+        assertLt(0, day.getDayOfWeek(), "Day of week should be greater than 0");
+    }
+}
