014153aa2c3f27667b176a8390478e24af7a4c1c
Elliot <elliotfriedman3@gmail.com>
2024-04-22 16:30:00 -0700

    add module and owners check to pre and post transaction to ensure no changes
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/TimeRestricted.sol
+++ src/TimeRestricted.sol
@@ -1,22 +1,67 @@
-pragma solidity 0.8.19;
+pragma solidity 0.8.25;
 
+import {BokkyPooBahsDateTimeLibrary} from "src/calendar/BokkyPooBahsDateTimeLibrary.sol";
 import {EnumerableSet} from "@openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol";
 import {BaseGuard} from "@safe/base/GuardManager.sol";
 import {Enum} from "@safe/common/Enum.sol";
+import {Safe} from "@safe/Safe.sol";
 
-import {BokkyPooBahsDateTimeLibrary} from "src/calendar/BokkyPooBahsDateTimeLibrary.sol";
+import {BytesHelper} from "src/BytesHelper.sol";
 
 /// @notice Only the timelock can add, edit, remove or disable
 /// time ranges after initialization.
 /// @title TimeRestricted contract that restricts
 /// contract interaction to a specific time range
+/// This guard also restricts changing owners and modules
+/// It enforce that the owners and modules
+/// remain the same after a transaction is executed
+/// If there is any changes, transactions are reverted
+
+/// Config:
+///  - the timelock must be a module of the safe to enact changes to the owners and modules
+///  - the safe must not be the only executor on the timelock, otherwise the safe could be
+///  locked out of making changes, except dark spells
+
+/// implements checks that no new modules were added or removed by a transaction
+/// uses TSTORE/TLOAD to store the current state of the modules
+/// iterate over modules starting at the sentinel address, then go through the linked list
+/// until the end is reached, storing the address of each module in a TSTORE slot
+/// store the number of modules in another TSTORE slot
+
+/// implement checks that no new owners were added or removed by a transaction
+/// uses getOwners() method for this
+/// then store the owners in TSTORE slots
+
+/// after the transaction in checkAfterExecution, check that the number of modules
+/// and the actual module addresses are the same
+/// check that the owners are the same
+
 contract TimeRestricted is BaseGuard {
+    using BytesHelper for bytes;
     using BokkyPooBahsDateTimeLibrary for uint256;
     using EnumerableSet for EnumerableSet.UintSet;
 
     /// @notice allowed days to interact with the contract
     mapping(address safe => EnumerableSet.UintSet allowedDay)
         private _allowedDays;
+
+    /// @notice storage slot for the guard
+    uint256 internal constant GUARD_STORAGE_SLOT =
+        0x4a204f620c8c5ccdca3fd54d003badd85ba500436a431f0cbda4f558c93c34c8;
+
+    /// @notice sentinel modules address
+    address internal constant SENTINEL_MODULES = address(0x1);
+
+    /// @notice TSTORE slot for the number of owners
+    uint256 public constant OWNER_LENGTH_SLOT =
+        uint256(keccak256("OWNER_LENGTH_SLOT"));
+
+    /// @notice TSTORE slot for the number of modules
+    uint256 public constant MODULE_LENGTH_SLOT =
+        uint256(keccak256("MODULE_LENGTH_SLOT"));
+
+    /// @notice the number of modules to retrieve in a single call
+    uint256 public constant PAGE_SIZE = 10;
 
     struct TimeRange {
         /// @notice start hour of the allowed time range
@@ -181,6 +226,8 @@ contract TimeRestricted is BaseGuard {
     /// to only a specific time range in specified days.
     /// no granularity to specify different hours for different allowed days.
     /// all allowed days have the same allowed hours.
+    /// Stores the current modules and owners in transient storage
+    /// If these change, then the transaction is reverted.
     function checkTransaction(
         address,
         uint256,
@@ -193,7 +240,7 @@ contract TimeRestricted is BaseGuard {
         address payable,
         bytes memory,
         address
-    ) external view {
+    ) external {
         /// If a safe has no allowed days, all actions are allowed
         /// once the safe has allowed days, transactions can only
         /// execute within the allowed hours.
@@ -204,15 +251,150 @@ contract TimeRestricted is BaseGuard {
             transactionAllowed(msg.sender, block.timestamp),
             "transaction outside of allowed hours"
         );
+
+        {
+            address[] memory owners = Safe(payable(msg.sender)).getOwners();
+            uint256 ownerLength = owners.length;
+            uint256 ownerSlot = OWNER_LENGTH_SLOT;
+
+            assembly {
+                tstore(ownerSlot, ownerLength)
+            }
+
+            for (uint256 i = 0; i < ownerLength; i++) {
+                uint256 ownerAddress = uint256(uint160(owners[i]));
+                // store owners in TSTORE slots
+                // store the number of owners in another TSTORE slot
+                assembly {
+                    tstore(ownerAddress, 1)
+                }
+            }
+        }
+
+        /// store modules in transient storage
+        /// store number of modules in transient storage
+        _traverseModules(SENTINEL_MODULES, tstoreValue, tstoreValue);
+    }
+
+    function tstoreTrue(uint256 slot) internal {
+        assembly {
+            tstore(slot, 1)
+        }
+    }
+
+    function tstoreValue(uint256 slot, uint256 value) internal {
+        assembly {
+            tstore(slot, value)
+        }
+    }
+
+    function checktTstoreValue(uint256 slot, uint256 value) internal view {
+        uint256 storedValue;
+        assembly {
+            storedValue := tload(slot)
+        }
+
+        require(storedValue == value, "TimeRestricted: value mismatch");
+    }
+
+    /// @notice recursively traverse through all modules by asking the Safe for its
+    /// modules in a paginated manner, storing the module addresses in TSTORE slots
+    /// and finally storing the total number of module addresses in a TSTORE slot.
+    /// Algorithm:
+    ///     query for 10 modules at a time, starting at sentinel address
+    ///     if the number of modules is less than 10, store the number of modules and total number of module addresses
+    ///     else, recurse with the next address and the total number of modules found so far
+    function _traverseModules(
+        address start,
+        function(uint256, uint256) internal moduleOperation,
+        function(uint256, uint256) internal moduleLengthOperation
+    ) internal returns (uint256 moduleAmountFound) {
+        (address[] memory modules, address next) = Safe(payable(msg.sender))
+            .getModulesPaginated(start, PAGE_SIZE);
+        uint256 moduleLength = modules.length;
+
+        for (uint256 i = 0; i < moduleLength; i++) {
+            uint256 moduleAddress = uint256(uint160(modules[i]));
+            // there should be no overlap between modules and owners so this operation is safe
+            // store modules in TSTORE slots
+            // store the number of modules in another TSTORE slot
+            moduleOperation(moduleAddress, 1);
+        }
+
+        /// if next == modules[modules.length - 1], we need to recurse
+        /// otherwise we are at the end of the modules list
+
+        /// if there are less than page_size modules, or the next is
+        /// the sentinel module, traversal ends
+        if (modules.length < PAGE_SIZE || next == SENTINEL_MODULES) {
+            moduleLengthOperation(
+                MODULE_LENGTH_SLOT,
+                moduleAmountFound + modules.length
+            );
+
+            return 0;
+        } else {
+            return
+                _traverseModules(next, moduleOperation, moduleLengthOperation) +
+                modules.length;
+        }
     }
 
     /// @notice no-op function, required by the Guard interface.
     /// No checks needed after the tx has been executed.
     /// The pre-checks are enough to ensure the transaction is valid.
-    function checkAfterExecution(bytes32, bool) external pure {}
+    function checkAfterExecution(bytes32, bool success) external {
+        /// if the transaction failed, no need to waste gas on further checks
+        if (!success) return;
+
+        bytes memory guardBytesPostExecution = Safe(payable(msg.sender))
+            .getStorageAt(GUARD_STORAGE_SLOT, 1);
+
+        address guardPostExecution = address(
+            uint160(uint256(guardBytesPostExecution.getFirstWord()))
+        );
+
+        require(
+            guardPostExecution == address(this),
+            "TimeRestricted: cannot remove guard"
+        );
+
+        address[] memory owners = Safe(payable(msg.sender)).getOwners();
+        uint256 ownerLength;
+        uint256 ownerSlot = OWNER_LENGTH_SLOT;
+
+        assembly {
+            ownerLength := tload(ownerSlot)
+        }
+
+        require(
+            ownerLength == owners.length,
+            "TimeRestricted: owners length changed"
+        );
+
+        for (uint256 i = 0; i < ownerLength; i++) {
+            uint256 ownerAddress = uint256(uint160(owners[i]));
+            uint256 found;
+            // retrieve whether owners were stored from TSTORE slots
+            assembly {
+                found := tload(ownerAddress)
+            }
+            require(found == 1, "TimeRestricted: owner not found");
+        }
+
+        /// check the modules in TSTORE slots, ensuring they are all 1
+        /// check the value of total modules in TSTORE slots, ensuring before and after is the same
+        /// if both of these are true, it means there were no changes made to the modules
+        _traverseModules(
+            SENTINEL_MODULES,
+            checktTstoreValue,
+            checktTstoreValue
+        );
+    }
 
     /// @notice callable by a safe, adds a time range to the allowed days
     /// for the safe to execute transactions
+    /// @param safe address of the safe to add the time range to
     /// @param dayOfWeek day of the week to allow transactions
     /// - valid range [1, 7]
     /// @param startHour start hour of the allowed time range
@@ -230,6 +412,7 @@ contract TimeRestricted is BaseGuard {
 
     /// @notice callable by a safe, adds a time range to the allowed days
     /// for the safe to execute transactions
+    /// @param safe address of the safe to add the time range to
     /// @param dayOfWeek day of the week to allow transactions
     /// - valid range [1, 7]
     /// @param startHour start hour of the allowed time range
@@ -308,6 +491,8 @@ contract TimeRestricted is BaseGuard {
     /// @notice remove an allowed day from the safe. This will remove
     /// the time range for the day. Cannot remove all allowed days.
     /// There must always be at least a single day allowed.
+    /// @param safe address of the safe to remove the allowed day from
+    /// @param dayOfWeek day of the week to remove
     function removeAllowedDay(
         address safe,
         uint8 dayOfWeek
@@ -336,6 +521,8 @@ contract TimeRestricted is BaseGuard {
     }
 
     /// @notice removes all time restrictions for the safe
+    /// callable only by the timelock
+    /// @param safe address of the safe to disable the guard for
     function disableGuard(address safe) external onlyTimelock(safe) {
         uint256[] memory allowedDays = _allowedDays[safe].values();
 
