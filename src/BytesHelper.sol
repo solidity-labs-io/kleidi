28a98d2f7f13750877cbd22666495c1ff2026993
Elliot <elliotfriedman3@gmail.com>
2024-04-29 00:46:24 -0700

    move away from silent failures
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/BytesHelper.sol
+++ src/BytesHelper.sol
@@ -4,14 +4,10 @@ library BytesHelper {
     /// @notice function to grab the first 4 bytes of calldata payload
     /// returns the function selector
     /// @param toSlice the calldata payload
-    function getFunctionSignature(bytes memory toSlice)
-        public
-        pure
-        returns (bytes4 functionSignature)
-    {
-        if (toSlice.length < 4) {
-            return bytes4(0);
-        }
+    function getFunctionSignature(
+        bytes memory toSlice
+    ) public pure returns (bytes4 functionSignature) {
+        require(toSlice.length >= 4, "No function signature");
 
         assembly {
             functionSignature := mload(add(toSlice, 0x20))
@@ -19,15 +15,10 @@ library BytesHelper {
     }
     /// @notice function to grab the first 32 bytes of returned memory
     /// @param toSlice the calldata payload
-
-    function getFirstWord(bytes memory toSlice)
-        public
-        pure
-        returns (uint256 value)
-    {
-        if (toSlice.length < 32) {
-            return 0;
-        }
+    function getFirstWord(
+        bytes memory toSlice
+    ) public pure returns (uint256 value) {
+        require(toSlice.length >= 32, "Length less than 32 bytes");
 
         assembly {
             value := mload(add(toSlice, 0x20))
@@ -39,17 +30,17 @@ library BytesHelper {
     /// @param toSlice the byte string to slice
     /// @param start the start index of the slice
     /// @param end the end index of the slice
-    function sliceBytes(bytes memory toSlice, uint256 start, uint256 end)
-        public
-        pure
-        returns (bytes memory)
-    {
+    function sliceBytes(
+        bytes memory toSlice,
+        uint256 start,
+        uint256 end
+    ) public pure returns (bytes memory) {
         require(
-            start <= toSlice.length,
+            start < toSlice.length,
             "Start index is greater than the length of the byte string"
         );
         require(
-            end <= toSlice.length,
+            end < toSlice.length,
             "End index is greater than the length of the byte string"
         );
         require(start < end, "Start index not less than end index");
