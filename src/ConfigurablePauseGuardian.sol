58d4d7b6d8af5d2eebc99b3ce79cb31f7c61a633
Elliot <34463580+ElliotFriedman@users.noreply.github.com>
2024-05-20 13:49:01 -0400

    Update src/ConfigurablePauseGuardian.sol
    Co-authored-by: Prateek Gupta <prateek105@users.noreply.github.com>

--- src/ConfigurablePauseGuardian.sol
+++ src/ConfigurablePauseGuardian.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.25;
 /// @notice possible states for this contract to be in:
 ///     1. paused, pauseStartTime != 0, guardian != address(0)
 ///     2. unpaused, pauseStartTime == 0, guardian != address(0)
-///     3. unpaused, pauseStartTime <= block.timestamp - pauseDuration, guardian != address(0)
+///     3. unpaused, pauseStartTime <= block.timestamp - pauseDuration, guardian == address(0)
 ///     4. unpaused after kick, pauseStartTime == 0, guardian == address(0)
 contract ConfigurablePauseGuardian {
     /// ---------------------------------------------------------
