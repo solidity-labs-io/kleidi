2830e2de08ac0f3903cb5a58e79c5c545285ff25
Elliot <elliotfriedman3@gmail.com>
2024-05-19 22:15:03 -0700

    addresses
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- test/utils/Addresses.sol
+++ test/utils/Addresses.sol
@@ -2,3 +2,15 @@
 pragma solidity 0.8.25;
 
 address constant MULTICALL3 = 0xcA11bde05977b3631167028862bE2a173976CA11;
+
+address constant logic = 0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552;
+
+address constant morphoBlue = 0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb;
+
+address constant ethenaUsd = 0x4c9EDD5852cd905f086C759E8383e09bff1E68B3;
+
+address constant dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+
+address constant irm = 0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC;
+
+address constant oracle = 0xaE4750d0813B5E37A51f7629beedd72AF1f9cA35;
