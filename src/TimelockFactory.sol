a1f31a2dfb2b702a1df1dac4a56650efa55d57ac
Elliot <elliotfriedman3@gmail.com>
2024-05-27 14:09:08 -0400

    timelock factory
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

---
+++ src/TimelockFactory.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {Timelock} from "src/Timelock.sol";
+
+/// @notice simple factory contract that creates timelocks for
+contract TimelockFactory {
+    /// ---------------------------------------------------------
+    /// ---------------------------------------------------------
+    /// -------------------- STORAGE VARIABLE -------------------
+    /// ---------------------------------------------------------
+    /// ---------------------------------------------------------
+
+    /// @notice mapping of timelock address to whether factory created
+    mapping(address timelock => bool created) public factoryCreated;
+
+    /// ---------------------------------------------------------
+    /// ---------------------------------------------------------
+    /// ------------------------- EVENT -------------------------
+    /// ---------------------------------------------------------
+    /// ---------------------------------------------------------
+
+    /// @notice Emitted when a call is scheduled as part of operation `id`.
+    /// @param timelock address of the newly created timelock
+    /// @param creationTime of the new timelock
+    /// @param sender that called the contract to create the timelock
+    event TimelockCreated(
+        address indexed timelock, uint256 indexed creationTime, address sender
+    );
+
+    /// @notice Initializes the contract with the following parameters:
+    /// @param _safe safe contract that owns this timelock
+    /// @param _minDelay initial minimum delay for operations
+    /// @param _expirationPeriod timelocked actions expiration period
+    /// @param _pauser address that can pause the contract
+    /// @param _pauseDuration duration the contract can be paused for
+    /// @param contractAddresses accounts that will have calldata whitelisted
+    /// @param selector function selectors to be whitelisted
+    /// @param startIndex start index of the calldata to be whitelisted
+    /// @param endIndex end index of the calldata to be whitelisted
+    /// @param data calldata to be whitelisted that resides between start and end index
+    function createTimelock(
+        address _safe,
+        uint256 _minDelay,
+        uint256 _expirationPeriod,
+        address _pauser,
+        uint128 _pauseDuration,
+        address[] memory contractAddresses,
+        bytes4[] memory selector,
+        uint16[] memory startIndex,
+        uint16[] memory endIndex,
+        bytes[] memory data
+    ) external returns (address timelock) {
+        timelock = address(
+            new Timelock(
+                _safe,
+                _minDelay,
+                _expirationPeriod,
+                _pauser,
+                _pauseDuration,
+                contractAddresses,
+                selector,
+                startIndex,
+                endIndex,
+                data
+            )
+        );
+
+        factoryCreated[timelock] = true;
+
+        emit TimelockCreated(timelock, block.timestamp, msg.sender);
+    }
+}
