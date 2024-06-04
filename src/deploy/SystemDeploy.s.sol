1cd46afc75c4231f438997a60179aa2fe4db3dc4
Elliot <elliotfriedman3@gmail.com>
2024-06-04 23:16:51 +0200

    remove addresses initialization from constructor, add deploy of RECOVERY_SPELL_FACTORY and INSTANCE_DEPLOYER
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/deploy/SystemDeploy.s.sol
+++ src/deploy/SystemDeploy.s.sol
@@ -7,17 +7,13 @@ import {Addresses} from "@forge-proposal-simulator/addresses/Addresses.sol";
 import {Timelock} from "src/Timelock.sol";
 import {TimeRestricted} from "src/TimeRestricted.sol";
 import {TimelockFactory} from "src/TimelockFactory.sol";
+import {InstanceDeployer} from "src/InstanceDeployer.sol";
+import {RecoverySpellFactory} from "src/RecoverySpellFactory.sol";
 
 /// @notice system deployment contract
-/// all contracts are permissionless and take no constructor params
-/// so there is nothing to validate
+/// all contracts are permissionless
 /// DO_PRINT=false DO_BUILD=false DO_RUN=false DO_DEPLOY=true DO_VALIDATE=true forge script src/deploy/SystemDeploy.s.sol:SystemDeploy --fork-url base -vvvvv
 contract SystemDeploy is MultisigProposal {
-    /// set addresses object in msig proposal
-    constructor() {
-        addresses = new Addresses("./Addresses.json");
-    }
-
     function name() public pure override returns (string memory) {
         return "SYS_DEPLOY";
     }
@@ -31,11 +27,28 @@ contract SystemDeploy is MultisigProposal {
             TimelockFactory factory = new TimelockFactory();
             addresses.addAddress("TIMELOCK_FACTORY", address(factory), true);
         }
+        if (!addresses.isAddressSet("RECOVERY_SPELL_FACTORY")) {
+            RecoverySpellFactory recoveryFactory = new RecoverySpellFactory();
+            addresses.addAddress(
+                "RECOVERY_SPELL_FACTORY", address(recoveryFactory), true
+            );
+        }
         if (!addresses.isAddressSet("TIME_RESTRICTED")) {
             TimeRestricted timeRestricted = new TimeRestricted();
             addresses.addAddress(
                 "TIME_RESTRICTED", address(timeRestricted), true
             );
+        }
+        if (!addresses.isAddressSet("INSTANCE_DEPLOYER")) {
+            InstanceDeployer deployer = new InstanceDeployer(
+                addresses.getAddress("SAFE_FACTORY"),
+                addresses.getAddress("SAFE_LOGIC"),
+                addresses.getAddress("TIMELOCK_FACTORY"),
+                addresses.getAddress("TIME_RESTRICTED"),
+                addresses.getAddress("MULTICALL3")
+            );
+
+            addresses.addAddress("INSTANCE_DEPLOYER", address(deployer), true);
         }
     }
 
@@ -53,6 +66,46 @@ contract SystemDeploy is MultisigProposal {
                 keccak256(restricted.code),
                 keccak256(type(TimeRestricted).runtimeCode),
                 "Incorrect TimeRestricted Bytecode"
+            );
+
+            address recoverySpellFactory =
+                addresses.getAddress("RECOVERY_SPELL_FACTORY");
+            assertEq(
+                keccak256(recoverySpellFactory.code),
+                keccak256(type(RecoverySpellFactory).runtimeCode),
+                "Incorrect RecoverySpellFactory Bytecode"
+            );
+
+            /// cannot check bytecode, following error is thrown when trying:
+            ///  `"runtimeCode" is not available for contracts containing
+            ///   immutable variables.`
+            InstanceDeployer deployer =
+                InstanceDeployer(addresses.getAddress("INSTANCE_DEPLOYER"));
+
+            assertEq(
+                deployer.safeProxyFactory(),
+                addresses.getAddress("SAFE_FACTORY"),
+                "incorrect safe proxy factory"
+            );
+            assertEq(
+                deployer.safeProxyLogic(),
+                addresses.getAddress("SAFE_LOGIC"),
+                "incorrect safe logic contract"
+            );
+            assertEq(
+                deployer.timelockFactory(),
+                addresses.getAddress("TIMELOCK_FACTORY"),
+                "incorrect timelock factory"
+            );
+            assertEq(
+                deployer.timeRestricted(),
+                addresses.getAddress("TIME_RESTRICTED"),
+                "incorrect TIME_RESTRICTED"
+            );
+            assertEq(
+                deployer.multicall3(),
+                addresses.getAddress("MULTICALL3"),
+                "incorrect MULTICALL3"
             );
         }
     }
