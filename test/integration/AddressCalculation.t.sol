cb4ca50aca62c3879e29d2083777884406b345bd
Elliot <elliotfriedman3@gmail.com>
2024-08-14 20:37:27 -0700

    test calculateAddressUnsafe
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- test/integration/AddressCalculation.t.sol
+++ test/integration/AddressCalculation.t.sol
@@ -228,4 +228,45 @@ contract AddressCalculationIntegrationTest is SystemIntegrationFixture {
         vm.expectRevert("InstanceDeployer: recovery spell has bytecode");
         addressCalculation.calculateAddress(instance);
     }
+
+    function testCalculateAddressUnsafe() public {
+        address[] memory recoverySpell = new address[](1);
+        recoverySpell[0] = recoverySpellAddress;
+
+        /// 2 / 3 multisig
+        uint256 threshold = 2;
+
+        DeploymentParams memory params = DeploymentParams({
+            minDelay: MINIMUM_DELAY + 1,
+            expirationPeriod: EXPIRATION_PERIOD,
+            pauser: guardian,
+            pauseDuration: PAUSE_DURATION,
+            hotSigners: hotSigners,
+            salt: salt
+        });
+
+        NewInstance memory instance = NewInstance({
+            owners: owners,
+            threshold: threshold,
+            recoverySpells: recoverySpell,
+            timelockParams: params
+        });
+
+        SystemInstance memory expectedContracts =
+            addressCalculation.calculateAddressUnsafe(instance);
+
+        SystemInstance memory actualContracts =
+            deployer.createSystemInstance(instance);
+
+        assertEq(
+            address(expectedContracts.safe),
+            address(actualContracts.safe),
+            "Safe address should match"
+        );
+        assertEq(
+            address(expectedContracts.timelock),
+            address(actualContracts.timelock),
+            "Timelock address should match"
+        );        
+    }
 }
