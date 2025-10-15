4b54a69d73e0a446ae2e5ea1a1c9ac625ed2119d
Elliot <elliotfriedman3@gmail.com>
2025-10-15 14:19:59 -0700

    update to new foundry comment style

--- src/utils/Constants.sol
+++ src/utils/Constants.sol
@@ -1,17 +1,17 @@
 pragma solidity 0.8.25;
 
-/// @dev timestamp indicating that an operation is done
+// timestamp indicating that an operation is done
 uint256 constant _DONE_TIMESTAMP = uint256(1);
 
-/// @dev minimum delay for timelocked operations
-/// @notice there is no minimum timelock duration enforced when creating a timelock,
-/// however it is recommended to set a timelock duration of at least a few hours as an end user
+// minimum delay for timelocked operations
+// there is no minimum timelock duration enforced when creating a timelock,
+// however it is recommended to set a timelock duration of at least a few hours as an end user
 uint256 constant MIN_DELAY = 1 days;
 
-/// @dev maximum delay for timelocked operations
+// maximum delay for timelocked operations
 uint256 constant MAX_DELAY = 30 days;
 
-/// @dev maximum number of timelocked operations scheduled at the same time
-/// @dev this is to prevent the contract from running out of gas when the pause
-/// function is called
+// maximum number of timelocked operations scheduled at the same time
+// this is to prevent the contract from running out of gas when the pause
+// function is called
 uint256 constant MAX_PROPOSAL_COUNT = 100;
