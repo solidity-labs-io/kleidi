e674ef35cc2f8b37272eca706701e151cfae0ded
solsr <elliotfriedman3@gmail.com>
2025-11-05 11:34:50 -0800

    update min delay to 10 seconds, redeploy across mainnet, base and optimism

--- src/utils/Constants.sol
+++ src/utils/Constants.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.25;
 uint256 constant _DONE_TIMESTAMP = uint256(1);
 
 /// @dev minimum delay for timelocked operations
-uint256 constant MIN_DELAY = 1 days;
+uint256 constant MIN_DELAY = 10 seconds;
 
 /// @dev maximum delay for timelocked operations
 uint256 constant MAX_DELAY = 30 days;
